92ee8f52b53256b67aa7971043f2dcab504344b6
blam92 <iraola.santi@gmail.com>
2017-12-18 18:43:50 -0800

    Added objectId to our message objects.

--- server/request-handler.js
+++ server/request-handler.js
@@ -1,3 +1,4 @@
+var url = require('url');
 /*************************************************************
 
 You should implement your request handler function in this file.
@@ -11,8 +12,64 @@ this file and include it in basic-server.js so that it actually works.
 *Hint* Check out the node module documentation at http://nodejs.org/api/modules.html.
 
 **************************************************************/
+var defaultCorsHeaders = {
+  'access-control-allow-origin': '*',
+  'access-control-allow-methods': 'GET, POST, PUT, DELETE, OPTIONS',
+  'access-control-allow-headers': 'content-type, accept',
+  'access-control-max-age': 10 // Seconds.
+};
+
+var idCounter = 2;
+
+let messages = {results: [
+  {
+    username: 'user',
+    text: 'test message 1',
+    roomname: 'floor 6',
+    objectId: 0
+  },
+  {
+    username: 'fishsticks',
+    text: 'test message 2',
+    roomname: 'floor 8',
+    objectId: 1
+  }
+]};
 
 var requestHandler = function(request, response) {
+  var headers = defaultCorsHeaders;
+  console.log(request.method, request.url);
+  if (request.method === 'GET' && request.url === '/classes/messages') {
+    var statusCode = 200;
+    headers['Content-Type'] = 'application/json';
+    response.writeHead(statusCode, headers);
+    // response.write(JSON.stringify(messages)); 
+    console.log('we are sending data!!');   
+    response.end(JSON.stringify(messages));
+  } else if (request.method === 'POST' && request.url === '/classes/messages') {
+    var statusCode = 201;
+    headers['Content-Type'] = 'application/json';
+    response.writeHead(statusCode, headers);
+    request.on('data', (message) => {
+      var q = url.parse(request.url + '/?' + message.toString(), true);
+      console.log(q);
+      console.log(q.query);
+      q.query.objectId = idCounter;
+      idCounter++;
+      messages.results.push(q.query);
+    });
+
+    request.on('end', () => {
+      response.end(JSON.stringify(messages));
+    }); 
+  } else if (request.method === 'OPTIONS' && request.url === '/classes/messages') {
+    response.writeHead(200, headers);
+    response.end();
+  
+  } else {
+    response.writeHead(404, headers);
+    response.end('Error');
+  }
   // Request and Response come from node's http module.
   //
   // They include information about both the incoming request, such as
@@ -27,23 +84,7 @@ var requestHandler = function(request, response) {
   // Adding more logging to your server can be an easy way to get passive
   // debugging help, but you should always be careful about leaving stray
   // console.logs in your code.
-  console.log('Serving request type ' + request.method + ' for url ' + request.url);
 
-  // The outgoing status.
-  var statusCode = 200;
-
-  // See the note below about CORS headers.
-  var headers = defaultCorsHeaders;
-
-  // Tell the client we are sending them plain text.
-  //
-  // You will need to change this if you are sending something
-  // other than plain text, like JSON or HTML.
-  headers['Content-Type'] = 'text/plain';
-
-  // .writeHead() writes to the request line and headers of the response,
-  // which includes the status and all headers.
-  response.writeHead(statusCode, headers);
 
   // Make sure to always call response.end() - Node may not send
   // anything back to the client until you do. The string you pass to
@@ -52,7 +93,6 @@ var requestHandler = function(request, response) {
   //
   // Calling .end "flushes" the response's internal buffer, forcing
   // node to actually send all the data over to the client.
-  response.end('Hello, World!');
 };
 
 // These headers will allow Cross-Origin Resource Sharing (CORS).
@@ -64,10 +104,7 @@ var requestHandler = function(request, response) {
 //
 // Another way to get around this restriction is to serve you chat
 // client from this domain by setting up static file serving.
-var defaultCorsHeaders = {
-  'access-control-allow-origin': '*',
-  'access-control-allow-methods': 'GET, POST, PUT, DELETE, OPTIONS',
-  'access-control-allow-headers': 'content-type, accept',
-  'access-control-max-age': 10 // Seconds.
-};
+
+
+module.exports.requestHandler = requestHandler;
 
